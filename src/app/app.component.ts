8355373711338db7868fd2a863687d54e7be3490
Larkin Family <larkinfamily@Larkins-iMac.local>
2016-05-09 22:59:13 -0500

    Added Http call for support example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,34 @@
 import {Component} from '@angular/core';
-
+import {NgFor, AsyncPipe} from '@angular/common';
 import {AppState} from './app_state.service.ts';
+import {Observable} from 'rxjs';
+
 
 @Component({
 	selector: 'app',
-	pipes: [],
+	pipes: [AsyncPipe],
 	providers: [],
-	directives: [],
+	directives: [NgFor],
 	styles: [],
-	template: `<div>Hello World</div>`
+	template: `
+    <div>Hello World</div>
+    <h1>Data Below</h1>
+    <ul>
+      <li></li>
+    </ul>
+  `
 })
 export class App {
+  public data: any;
 	onLoadWelcomeMessage: string = `Hello ngConf! I'm logging on ngOnInit()`;
 	name: string = 'Angular2 Webpack Lite';
 
-	constructor(public appState: AppState) {}
+	constructor(public appState: AppState) {
+    this.appState.getJsonData().subscribe(
+      (data) => {debugger},
+      (err) => {debugger}
+    );
+  }
 
 	ngOnInit() {
 		console.log(this.onLoadWelcomeMessage, `App state is ${this.appState.state}`);
--- src/app/app_state.service.ts
+++ src/app/app_state.service.ts
@@ -1,11 +1,15 @@
 /* Thanks from @AngularClass */
 import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
 
+@Injectable()
 export class AppState {
   _state = {};
 
-  constructor() {}
-  
+  constructor(private http: Http) {
+
+  }
+
   // already return a clone of the current state
   get state() {
     return this._state = this._clone(this._state);
@@ -15,20 +19,25 @@ export class AppState {
     throw new Error('do not mutate the `.state` directly');
   }
 
-  get(prop?: any) {
-    // use our state getter for the clone
-    const state = this.state;
-    return state[prop] || state;
-  }
+  // get(prop?: any) {
+  //   // use our state getter for the clone
+  //   const state = this.state;
+  //   return state[prop] || state;
+  // }
 
-  set(prop: string, value: any) {
-    return this._state[prop] = value;
-  }
-
+  // set(prop: string, value: any) {
+  //   return this._state[prop] = value;
+  // }
 
   _clone(object) {
     return JSON.parse(JSON.stringify( object ));
   }
 
+
+  // For FOXJAZZ HTTP
+  getJsonData() {
+    return this.http.get('file.json')
+      .map(response => {return response.json()});
+  }
 
 }
